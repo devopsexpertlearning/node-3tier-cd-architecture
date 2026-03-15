8487657898740306a5ea3693c177226531097dcb
devopsexpertlearning <mail@devopsexpert.work.gd>
2026-03-15 19:01:12 +0530

    normalize line endings and add gitattributes

--- terraform/modules/fargate-profile/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "fargate_profile_ids" {
-  description = "A map of Fargate Profile IDs"
-  value       = { for k, v in aws_eks_fargate_profile.this : k => v.id }
-}
-
-output "fargate_profile_arns" {
-  description = "A map of Fargate Profile ARNs"
-  value       = { for k, v in aws_eks_fargate_profile.this : k => v.arn }
-}
--- terraform/modules/security-groups/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "security_group_ids" {
-  description = "Map of all security group IDs by key"
-  value       = { for k, sg in aws_security_group.main : k => sg.id }
-}
